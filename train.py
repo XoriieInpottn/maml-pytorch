d5431bed91622ba83ae80be35c9f6c24f5eec426
XoriieInpottn <XoriieInpottn@126.com>
2022-08-24 20:10:07 +0800

    optimize the code

--- train.py
+++ train.py
@@ -19,15 +19,15 @@ from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-import utils
 from dataset import TrainDataset, TestDataset
 from maml import MAML
 from model import Model
+from utils import BaseConfig, CosineWarmUpAnnealingLR
 
 cv.setNumThreads(0)
 
 
-class Config(object):
+class Config(BaseConfig):
 
     def __init__(self):
         self.train_path = None
@@ -53,13 +53,6 @@ class Config(object):
 
         self.device = 'cpu'
 
-    def load(self, args):
-        for name in dir(self):
-            if name.startswith('_') or callable(getattr(self, name)):
-                continue
-            if hasattr(args, name):
-                setattr(self, name, getattr(args, name))
-
 
 class Trainer(object):
 
@@ -69,20 +62,20 @@ class Trainer(object):
             loss_fn: Callable,
             config: Config
     ) -> None:
-        self._model = model
-        self._loss_fn = loss_fn
-        self._args = config
+        self.model = model
+        self.loss_fn = loss_fn
+        self.config = config
 
-        self._model = model.to(self._args.device)
-        if isinstance(self._loss_fn, nn.Module):
-            self._loss_fn.to(self._args.device)
+        self.model = model.to(self.config.device)
+        if isinstance(self.loss_fn, nn.Module):
+            self.loss_fn.to(self.config.device)
 
         self._maml = MAML(
-            self._model,
-            loss_fn=self._loss_fn,
-            inner_lr=self._args.inner_lr,
-            num_steps=self._args.num_steps,
-            first_order=self._args.first_order
+            self.model,
+            loss_fn=self.loss_fn,
+            inner_lr=self.config.inner_lr,
+            num_steps=self.config.num_steps,
+            first_order=self.config.first_order
         )
 
         self._create_dataset()
@@ -91,13 +84,13 @@ class Trainer(object):
     def _create_dataset(self):
         self._train_loader = DataLoader(
             TrainDataset(
-                self._args.train_path,
-                image_size=self._args.image_size,
-                num_ways=self._args.num_ways,
-                num_shots=self._args.num_shots,
-                size=self._args.train_size
+                self.config.train_path,
+                image_size=self.config.image_size,
+                num_ways=self.config.num_ways,
+                num_shots=self.config.num_shots,
+                size=self.config.train_size
             ),
-            batch_size=self._args.batch_size,
+            batch_size=self.config.batch_size,
             shuffle=True,
             drop_last=True,
             persistent_workers=True,
@@ -106,26 +99,26 @@ class Trainer(object):
         )
         self._test_loader = DataLoader(
             TestDataset(
-                self._args.test_path,
-                image_size=self._args.image_size,
-                num_ways=self._args.num_ways,
-                num_shots=self._args.num_shots,
-                size=self._args.test_size
+                self.config.test_path,
+                image_size=self.config.image_size,
+                num_ways=self.config.num_ways,
+                num_shots=self.config.num_shots,
+                size=self.config.test_size
             ),
-            batch_size=self._args.batch_size,
+            batch_size=self.config.batch_size,
             num_workers=8,
             pin_memory=True
         )
 
     def _create_optimizer(self):
-        self._parameters = list(self._model.parameters())
-        optimizer_class = getattr(optim, self._args.optimizer)
+        self._parameters = list(self.model.parameters())
+        optimizer_class = getattr(optim, self.config.optimizer)
         opt_args = {
             'params': self._parameters,
-            'lr': self._args.max_lr,
-            'weight_decay': self._args.weight_decay,
-            'momentum': self._args.momentum,  # SGD, RMSprop
-            'betas': (self._args.momentum, 0.999),  # Adam*
+            'lr': self.config.max_lr,
+            'weight_decay': self.config.weight_decay,
+            'momentum': self.config.momentum,  # SGD, RMSprop
+            'betas': (self.config.momentum, 0.999),  # Adam*
         }
         co = optimizer_class.__init__.__code__
         self._optimizer = optimizer_class(**{
@@ -133,8 +126,8 @@ class Trainer(object):
             for name in co.co_varnames[1:co.co_argcount]
             if name in opt_args
         })
-        num_loops = self._args.num_epochs * len(self._train_loader)
-        self._scheduler = utils.CosineWarmUpAnnealingLR(self._optimizer, num_loops)
+        num_loops = self.config.num_epochs * len(self._train_loader)
+        self._scheduler = CosineWarmUpAnnealingLR(self._optimizer, num_loops)
 
     def _train_step(self, support_x, support_y, query_x, query_y):
         """Train with a batch of tasks. Each task is consist of several samples.
@@ -148,14 +141,14 @@ class Trainer(object):
         Returns:
             tuple[torch.Tensor, int]: loss and the current learning rate
         """
-        support_x = support_x.to(self._args.device)
-        support_y = support_y.to(self._args.device)
-        query_x = query_x.to(self._args.device)
-        query_y = query_y.to(self._args.device)
+        support_x = support_x.to(self.config.device)
+        support_y = support_y.to(self.config.device)
+        query_x = query_x.to(self.config.device)
+        query_y = query_y.to(self.config.device)
 
         loss = self._maml(support_x, support_y, query_x, query_y)
         loss.backward()
-        clip_grad_norm_(self._model.parameters(), 0.1, inf)
+        clip_grad_norm_(self.model.parameters(), 0.1, inf)
         self._optimizer.step()
         self._optimizer.zero_grad(set_to_none=True)
         self._scheduler.step()
@@ -172,25 +165,25 @@ class Trainer(object):
         Returns:
             torch.Tensor: query_y (batch_size, num_samples, ...)
         """
-        support_x = support_x.to(self._args.device)
-        support_y = support_y.to(self._args.device)
-        query_x = query_x.to(self._args.device)
+        support_x = support_x.to(self.config.device)
+        support_y = support_y.to(self.config.device)
+        query_x = query_x.to(self.config.device)
 
         qy_list = []
         self._maml.checkpoint()
         for sx, sy, qx in zip(support_x, support_y, query_x):
-            for i in range(self._args.num_steps * 2):
-                pred = self._model(sx)
-                loss = self._loss_fn(pred, sy)
+            for i in range(self.config.num_steps * 2):
+                pred = self.model(sx)
+                loss = self.loss_fn(pred, sy)
                 loss.backward()
                 with torch.no_grad():
-                    for p in self._model.parameters():
+                    for p in self.model.parameters():
                         if not p.requires_grad or p.grad is None:
                             continue
-                        p.add_(p.grad, alpha=-self._args.inner_lr)
+                        p.add_(p.grad, alpha=-self.config.inner_lr)
                         p.grad = None
 
-            pred = self._model(qx)
+            pred = self.model(qx)
             qy = torch.argmax(pred, 1)
             qy_list.append(qy)
 
@@ -200,7 +193,7 @@ class Trainer(object):
 
     def train(self):
         loss_g = None
-        for epoch in range(self._args.num_epochs):
+        for epoch in range(self.config.num_epochs):
             with tqdm(total=len(self._train_loader), leave=False, ncols=96) as loop:
                 for support_doc, query_doc in self._train_loader:
                     loop.update()
@@ -213,7 +206,7 @@ class Trainer(object):
                     loss = float(loss.numpy())
                     loss_g = 0.9 * loss_g + 0.1 * loss if loss_g is not None else loss
                     loop.set_description(
-                        f'[{epoch + 1}/{self._args.num_epochs}] '
+                        f'[{epoch + 1}/{self.config.num_epochs}] '
                         f'L={loss_g:.06f} '
                         f'lr={lr:.01e}',
                         False
@@ -221,7 +214,7 @@ class Trainer(object):
 
             acc = self._evaluate()
             print(
-                f'[{epoch + 1}/{self._args.num_epochs}] '
+                f'[{epoch + 1}/{self.config.num_epochs}] '
                 f'L={loss_g:.06f} '
                 f'acc={acc:.02%} '
             )
@@ -274,6 +267,8 @@ def main():
     config.device = 'cuda' if torch.cuda.is_available() else 'cpu'
     config.train_path = [os.path.join(args.data_path, 'train.ds'), os.path.join(args.data_path, 'valid.ds')]
     config.test_path = os.path.join(args.data_path, 'test.ds')
+
+    print(config)
 
     model = Model(
         args.image_size,
--- utils.py
+++ utils.py
@@ -4,10 +4,45 @@
 @author: Guangyi
 @since: 2021-06-29
 """
-
+import io
 import math
 
 from torch.optim.lr_scheduler import LambdaLR
+
+
+class BaseConfig(object):
+
+    def load(self, args):
+        for name, value in self.__dict__.items():
+            if name.startswith('_') or callable(value):
+                continue
+            if hasattr(args, name):
+                setattr(self, name, getattr(args, name))
+
+    def __str__(self):
+        pairs = []
+        max_len = 0
+        for name, value in self.__dict__.items():
+            if name.startswith('_') or callable(value):
+                continue
+            if value is None:
+                value = 'None'
+            elif isinstance(value, str):
+                value = f'\'{value}\''
+            else:
+                value = str(value)
+            l = len(name)
+            if l > max_len:
+                max_len = l
+            pairs.append((name, value))
+
+        buffer = io.StringIO()
+        for name, value in pairs:
+            buffer.write(name.rjust(max_len))
+            buffer.write(': ')
+            buffer.write(value)
+            buffer.write('\n')
+        return buffer.getvalue()
 
 
 class CosineWarmUpAnnealingLR(LambdaLR):
